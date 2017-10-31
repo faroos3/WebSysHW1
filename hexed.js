28f546e256eef7963bade2dc9ec32b0782a882c9
Raz Reed <raz@shipstone.com>
2017-10-31 18:19:40 -0400

    fixed initialization of Date object

--- hexed.js
+++ hexed.js
@@ -15,7 +15,7 @@
       // score the user
       var diff = document.getElementById("diff"); // difficulty
       var err = (abs(R-ansR)+abs(G-ansG)+abs(B-absB))*100/255/3; // error of user's guess
-      $("#score").text(((15000-Date.now()-startTime)*(15-diff-err)/(15-diff)).toFixed(2));
+      $("#score").text(((15000- new Date().now()-startTime)*(15-diff-err)/(15-diff)).toFixed(2));
       // note: assuming #score is an actual element and that startTime is defined
       return false;
     });
