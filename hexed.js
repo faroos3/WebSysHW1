18aacb2803f088e871a34162af4cf10225ae97a4
aikenj <aikenj@rpi.edu>
2017-10-31 17:50:15 -0400

    reads form data on submit

--- hexed.js
+++ hexed.js
@@ -1,10 +1,22 @@
 (function($) {
-	$.fn.hexed = function() {
-		gameHTML = "front end guys will make this";
-		this.append(gameHTML);
-	};
+  $.fn.hexed = function() {
+    gameHTML = "front end guys will make this";
+    var R = Math.floor((Math.random() * 255) + 0);
+    var G = Math.floor((Math.random() * 255) + 0);
+    var B = Math.floor((Math.random() * 255) + 0);
+
+    this.append(gameHTML+"R: "+R.toString()+" G: "+G.toString()+" B "+B.toString());
+    
+
+    $("#user_input").submit( function() {
+      var ansR = $('input[id="R"]').val();
+      var ansG = $('input[id="G"]').val();
+      var ansB = $('input[id="B"]').val();
+      return false;
+    });
+  };
 }(jQuery));
 
 $(document).ready(function() {
-	$("#hexed").hexed();
+  $("#hexed").hexed();
 });
