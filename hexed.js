d26552d1eaeac348abea69a761f0ef7f6b097bad
hux8 <hux8@rpi.edu>
2017-11-05 22:00:47 -0500

    FINAL VERSION

--- hexed.js
+++ hexed.js
@@ -1,4 +1,12 @@
 (function($) {
+  var seconds_int = 0; 
+    var minutes_int = 0;
+    var current_round = 1; 
+    var myInterval = null; 
+    var clicked = true;
+    var score = 0;
+    var turns = 1;
+    // var babby = true;
   $.fn.hexed = function(settings) {
     gameHTML = '<div id="theGame" class="ui-widget-content">';
     gameHTML += '<div id="colorBlock"></div>';
@@ -14,12 +22,111 @@
     gameHTML += '</div>';
     gameHTML += '<div id="result_submit">';
     gameHTML += '<div id="result">';
-    gameHTML += '<div id="round">Round 1 of 10</div>';
+    gameHTML += '<div id="round">Round ' + turns + ' of ';
+    gameHTML += settings.turns;
+    gameHTML += '</div>';
     gameHTML += '<div id="score">Score: </div>';
     gameHTML += '</div>';
+    gameHTML += '<button><a href="">Restart!</a></button>';
     gameHTML += '</div>';
 
     $('#gameBlock').html(gameHTML);
+    $( function() {
+      $("#user_input").submit(function(e) {
+          e.preventDefault();
+      });
+
+      $( "#red" ).slider({
+        orientation: "horizontal",
+        max: 255,
+        value: 127,
+        range: "min",
+        slide: function( event, ui) {
+          var value = ui.value; 
+          var value = value.toString(16);
+          if (value.length < 2) { // if the value is less then 2 digits, add a 0 to the front
+          $("#red-input").val("0" + value);
+        } else {
+          $("#red-input").val(value);
+        }
+        }
+      });   
+
+      $( "#green" ).slider({
+        orientation: "horizontal",
+        range: "min",
+        max: 255,
+        value: 127,
+        slide: function(event, ui) {
+          var value = ui.value;
+          var value = value.toString(16);
+          if (value.length < 2) {
+          $("#green-input").val("0" + value);
+        } else {
+          $("#green-input").val(value);
+        }
+        }
+      });      
+
+      $( "#blue" ).slider({
+        orientation: "horizontal",
+        range: "min",
+        max: 255,
+        value: 127,
+        slide: function(event, ui) {
+          var value = ui.value;
+          var value = value.toString(16);
+          if (value.length < 2) {
+          $("#blue-input").val("0" + value);
+        } else {
+          $("#blue-input").val(value);
+        }
+        }
+      });  
+
+      $("#red-input").change(function() {//if user changed the input, change the slider
+        var value = parseInt (this.value, 16)//convert hex to decimal(RGB)
+        console.log(this.value); //debug purpose
+        $("#red").slider("value", value);
+      });
+
+      $("#green-input").change(function() {
+        var value = parseInt (this.value, 16)
+        console.log(this.value); 
+        $("#green").slider("value", value);
+      });
+
+      $("#blue-input").change(function() {
+        var value = parseInt (this.value, 16)
+        console.log(this.value); 
+        $("#blue").slider("value", value);
+      });
+
+      } );
+    
+    //var myInterval = setInterval(for_seconds, 1000); 
+    function for_seconds(){
+      seconds_int += 1; 
+      
+      document.getElementById("seconds").innerHTML = seconds_int + " s";
+
+      if (seconds_int%60 == 0){
+        minutes_int += 1;
+        //document.getElementById("minutes").innerHTML = minutes_int + " m";
+      }
+    }
+    document.getElementById("start").onclick = function(){
+      // if (clicked) {
+        seconds_int = 0; 
+        minutes_int = 0; 
+        clearInterval(myInterval);
+        document.getElementById("seconds").innerHTML = seconds_int + " s";
+        //document.getElementById("minutes").innerHTML = minutes_int + " m";
+        myInterval = setInterval(for_seconds, 1000); 
+        // clicked = false;
+      // }
+    }; 
+
     startTime = Date.now();
     var R = Math.floor((Math.random() * 256));
     var G = Math.floor((Math.random() * 256));
@@ -39,18 +146,23 @@
       if(val < 0) {
         val = 0;
       }
+      score += val;
       $("#score").text("Score: " + val);
-      //this.append((15000 - (endTime-startTime))+' x '+(15-settings.diff-err)+' / '+(15-settings.diff));
-
+      // if (babby) {
+        ++turns;
+        // babby = false;
+      // }
       // note: assuming #score is an actual element and that startTime is defined
       return false;
     });
   };
 }(jQuery));
 $(document).ready(function() {
-	$("#hexed").hexed({
-    diff: parseInt($("#diff").val()),
-    turns: parseInt($("#rounds").val())
+	$("#hexed").click(function() {
+    $("#gameBlock").hexed({
+      diff: parseInt($("#diff").val()),
+      turns: parseInt($("#round").val())
+    });
   });
 });
 
