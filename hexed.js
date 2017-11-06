76ddaacf4185d50f5f8b647cade27acef3bbcef6
hux8 <hux8@rpi.edu>
2017-11-05 20:52:25 -0500

    Add files via upload

--- hexed.js
+++ hexed.js
@@ -6,22 +6,21 @@
     var G = Math.floor((Math.random() * 256));
     var B = Math.floor((Math.random() * 256));
 
-    this.append(gameHTML+" R: "+R.toString(16)+" G: "+G.toString(16)+" B: "+B.toString(16));
+    // this.append(gameHTML+" R: "+R.toString(16)+" G: "+G.toString(16)+" B: "+B.toString(16));
     $("#colorBlock").css("background-color","#"+R.toString(16)+G.toString(16)+B.toString(16));
 
     $("#user_input").submit( function() {
-      var ansR = parseInt($('#R').val(), 16);
-      var ansG = parseInt($('#G').val(), 16);
-      var ansB = parseInt($('#B').val(), 16);
+      var ansR = parseInt($('#red-input').val(), 16);
+      var ansG = parseInt($('#green-input').val(), 16);
+      var ansB = parseInt($('#blue-input').val(), 16);
       // score the user
-      // var diff = parseInt($("#diff").val()); // difficulty
       var err = (Math.abs(R-ansR)+Math.abs(G-ansG)+Math.abs(B-ansB))*100/255/3; // error of user's guess
       var endTime = Date.now();
       var val = ((15000- (endTime-startTime))*(15-settings.diff-err)/(15-settings.diff)).toFixed(2);
       if(val < 0) {
         val = 0;
       }
-      $("#score").text(val);
+      $("#score").text("Score: " + val);
       //this.append((15000 - (endTime-startTime))+' x '+(15-settings.diff-err)+' / '+(15-settings.diff));
 
       // note: assuming #score is an actual element and that startTime is defined
